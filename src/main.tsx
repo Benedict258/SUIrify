680482b6b0ce09015a4750e1e7ba4dc9f3dc56e1
Benedict258 <isaacbenedict001@gmail.com>
2025-10-28 16:18:34 +0100

    BEFORE BOLTTING

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,36 @@
-import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  createNetworkConfig,
+  SuiClientProvider,
+  WalletProvider,
+} from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui/client";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// --- Configuration ---
+const { networkConfig } = createNetworkConfig({
+  localnet: { url: getFullnodeUrl("localnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+});
+
+const queryClient = new QueryClient();
+
+// --- Wrap the entire app ---
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <SuiClientProvider networks={networkConfig} defaultNetwork="testnet">
+          <WalletProvider autoConnect>
+            <App />
+          </WalletProvider>
+        </SuiClientProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
